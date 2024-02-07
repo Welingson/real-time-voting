959ea817c66844db54a50a5daf95ad511566e2d6
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-02-07 11:45:57 -0300

    feat: add prisma orm

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,29 @@
-console.log('Hello world');
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+
+const server = fastify();
+const prisma = new PrismaClient();
+
+
+server.post('/polls', async (request, reply) => {
+
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body);
+
+    const poll = await prisma.poll.create({
+        data: {
+            title: title,
+        }
+    })
+
+    reply.status(201).send(poll);
+
+})
+
+server.listen({ port: 3333 }).then(() => {
+    console.log('server running!');
+})

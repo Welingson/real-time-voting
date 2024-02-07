a6c0d1f8c611fa9363822377a78a3bd79d8b4b48
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-02-07 17:04:33 -0300

    feat: create vote

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+
+export async function getPoll(server: FastifyInstance) {
+    server.get('/polls/:pollId', async (request, reply) => {
+
+        const getPollParams = z.object({
+            pollId: z.string().uuid()
+        })
+
+        const { pollId } = getPollParams.parse(request.params);
+
+        const poll = await prisma.poll.findUnique({
+            where:{
+                id: pollId
+            },
+            include:{
+                PollOption:{
+                    select:{
+                        id: true,
+                        title: true
+                    }
+                }
+            }
+        })
+
+        reply.send(poll);
+    })
+
+}

7e1648b0d72972c6151420b34dd4a711de778bfa
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-02-07 12:48:10 -0300

    refactor: code organization

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,10 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+
+import { createPoll } from './routes/create-poll';
 
 const server = fastify();
-const prisma = new PrismaClient();
 
-
-server.post('/polls', async (request, reply) => {
-
-    const createPollBody = z.object({
-        title: z.string()
-    })
-
-    const { title } = createPollBody.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title: title,
-        }
-    })
-
-    reply.status(201).send(poll);
-
-})
+server.register(createPoll)
 
 server.listen({ port: 3333 }).then(() => {
     console.log('server running!');

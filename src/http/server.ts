291deef6508b39a4f0db7ac7c096df29e2a90d92
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-02-07 17:04:44 -0300

    feat: add cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,20 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const server = fastify();
 
+server.register(cookie, {
+    secret: "real-time-voting",
+    hook: 'onRequest'
+});
+
 server.register(createPoll)
+server.register(getPoll)
+server.register(voteOnPoll)
 
 server.listen({ port: 3333 }).then(() => {
     console.log('server running!');
